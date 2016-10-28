54d8e6a3e07560c4571981ab1f6609080d7a88af
Ahmed Mohamed <a7mad.mohammad@gmail.com>
2016-10-28 02:39:37 +0200

    2.4differences in activity patterns between weekdays and weekends.R

---
+++ 2.4differences in activity patterns between weekdays and weekends.R
@@ -0,0 +1,32 @@
+echo = TRUE
+
+activity <- read.csv("/Volumes/Files /Coursera/Data Science /reproducible-research/Week2 Assignment/activity.csv", header = T, sep = ",")
+
+activity2$weekday <- c("weekday")
+activity2[weekdays(as.Date(activity2[, 2])) %in% c("Saturday", "Sunday", "samedi", "dimanche", "saturday", "sunday", "Samedi", "Dimanche"), ][4] <- c("weekend")
+table(activity2$weekday == "weekend")
+activity2$weekday <- factor(activity2$weekday)
+
+activity2_weekend <- subset(activity2, activity2$weekday == "weekend")
+activity2_weekday <- subset(activity2, activity2$weekday == "weekday")
+
+mean_activity2_weekday <- tapply(activity2_weekday$steps, activity2_weekday$interval, mean)
+mean_activity2_weekend <- tapply(activity2_weekend$steps, activity2_weekend$interval, mean)
+ 
+# FALSE  TRUE 
+# 12960  4608
+activity2$weekday <- factor(activity2$weekday)
+
+activity2_weekend <- subset(activity2, activity2$weekday == "weekend")
+activity2_weekday <- subset(activity2, activity2$weekday == "weekday")
+
+mean_activity2_weekday <- tapply(activity2_weekday$steps, activity2_weekday$interval, mean)
+mean_activity2_weekend <- tapply(activity2_weekend$steps, activity2_weekend$interval, mean)
+
+df_weekday <- data.frame(interval = unique(activity2_weekday$interval), avg = as.numeric(mean_activity2_weekday), day = rep("weekday", length(mean_activity2_weekday)))
+df_weekend <- data.frame(interval = unique(activity2_weekend$interval), avg = as.numeric(mean_activity2_weekend), day = rep("weekend", length(mean_activity2_weekend)))
+df_final <- rbind(df_weekday, df_weekend)
+
+xyplot(avg ~ interval | day, data = df_final, layout = c(1, 2), 
+       type = "l", ylab = "Number of steps")
+       
